902f8c7092e06f5346113201f297539eeb2468e4
Olga Matskevich <olmatske@1-E-6.42heilbronn.de>
2026-03-03 18:55:50 +0100

    added prompts and andsweres to name

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 )
-
 // Send any text message to the bot after the bot has been started
 // 'token:=' is the assignment of the correct type, otherwise it would be e.g. 'token string = ...'
+
+var userState = make(map[int64]string)
 
 func main() {
 	token:= os.Getenv("TOKEN")
@@ -30,11 +31,55 @@ func main() {
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID: update.Message.Chat.ID,
-		Text:   update.Message.Text,
-	})
+	
+	if update.Message == nil {
+		return
+	}
+
+	chatID:= update.Message.Chat.ID
+	text:=    update.Message.Text
+
+	switch userState[chatID]{
+	case "":
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: chatID,
+			Text: "Hi, what's your Name?",
+		})
+		userState[chatID] = "await.name"
+
+	case "await.name":
+		name := text
+		if name == "Noel" {
+			b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: chatID,
+				Text: "Hope you're having a great day, I love you! ❤️",
+			})
+		} else if name == "Jonas" {
+			b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: chatID,
+				Text: "Servus Tudn 😎",
+			})
+		} else if name == "Milena" {
+			b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: chatID,
+				Text: "Wanna grab a coffee sometime? ☕️",
+			})
+		} else {
+			b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: chatID,
+				Text: "Hi " + name + "!",
+			})
+		}
+		userState[chatID] = ""
+	}
 }
+
+
+	// b.SendMessage(ctx, &bot.SendMessageParams{
+	// 	ChatID: update.Message.Chat.ID,
+	// 	Text:   "",
+	// })
+// }
 
 
 
